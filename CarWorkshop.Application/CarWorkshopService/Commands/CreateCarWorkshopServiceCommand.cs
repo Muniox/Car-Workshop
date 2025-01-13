c6832f369f9cd45cd8eebc026127d3a9cb7f7e00
Muniox <muniox20@gmail.com>
2025-01-13 22:45:28 +0100

    Add Razor view for CreateCarWorkshopServiceCommand

---
+++ CarWorkshop.Application/CarWorkshopService/Commands/CreateCarWorkshopServiceCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace CarWorkshop.Application.CarWorkshopService.Commands
+{
+    public class CreateCarWorkshopServiceCommand : CarWorkshopServiceDto, IRequest
+    {
+        public string CarWorkshopEndodedName { get; set; } = default!;
+    }
+}
