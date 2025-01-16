f8cf8c68ec6f9a8f9dc85fb98667fbbc2c802112
Muniox <muniox20@gmail.com>
2025-01-16 22:25:45 +0100

    Added tests for domain entities

---
+++ CarWorkshop.Domain.Tests/Entities/CarWorkshopTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CarWorkshop.Domain.Entities.Tests
+{
+    public class CarWorkshopTests
+    {
+        [Fact()]
+        public void EncodeName_ShouldSetEncodedName()
+        {
+            // arrange
+            var carWorkshop = new CarWorkshop();
+            carWorkshop.Name = "Test Workshop";
+
+            // act
+            carWorkshop.EncodeName();
+
+            // assert
+            carWorkshop.EncodedName.Should().Be("test-workshop"); // fluent assertion jest płatne teraz XDD należy używać wersji 7 https://www.youtube.com/watch?v=ZFc6jcaM6Ms&t=300s
+            //Xunit.Assert.Equivalent(carWorkshop.EncodedName, "test-workshop"); // albo nie używać
+        }
+
+        [Fact()]
+        public void EndodeName_ShouldThrowException_WhenNameIsNull()
+        {
+            // arrange
+            var carWorkshop = new CarWorkshop();
+
+            // act
+            Action action = () => carWorkshop.EncodeName();
+
+            // arrange
+            action.Invoking(a => a.Invoke()).Should().Throw<NullReferenceException>();
+        }
+    }
+}
