d0f1cc8ba844cc03eebc1f2f3020456d3d4bab72
Muniox <muniox20@gmail.com>
2025-01-18 22:43:56 +0100

    Validator test

--- CarWorkshop.Application.Tests/ApplicationUser/CurrentUserTests.cs
+++ CarWorkshop.Application.Tests/ApplicationUser/CurrentUserTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using FluentAssertions;
+using Xunit;
 
 namespace CarWorkshop.Application.ApplicationUser.Tests
 {
@@ -7,7 +8,49 @@ namespace CarWorkshop.Application.ApplicationUser.Tests
         [Fact()]
         public void IsInARole_WithMatchingRole_ShouldReturnTrue()
         {
+            // arrange
 
+            var currentUser = new CurrentUser("1", "test@tests.com", new List<string> { "Admin", "User" });
+
+            // act
+
+            var isAInRole = currentUser.IsAInRole("Admin");
+
+            // assert
+
+            isAInRole.Should().BeTrue();
+        }
+
+        [Fact()]
+        public void IsInARole_WithMatchingRole_ShouldReturnFalse()
+        {
+            // arrange
+
+            var currentUser = new CurrentUser("1", "test@tests.com", new List<string> { "Admin", "User" });
+
+            // act
+
+            var isAInRole = currentUser.IsAInRole("Manager");
+
+            // assert
+
+            isAInRole.Should().BeFalse();
+        }
+
+        [Fact()]
+        public void IsInARole_WithNonMatchingCaseRole_ShouldReturnFalse()
+        {
+            // arrange
+
+            var currentUser = new CurrentUser("1", "test@tests.com", new List<string> { "Admin", "User" });
+
+            // act
+
+            var isAInRole = currentUser.IsAInRole("admin");
+
+            // assert
+
+            isAInRole.Should().BeFalse();
         }
     }
 }
---
+++ CarWorkshop.Application.Tests/CarWorkshopService/Commands/CreateCarWorkshopServiceCommandValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation.TestHelper;
+using Xunit;
+
+namespace CarWorkshop.Application.CarWorkshopService.Commands.Tests
+{
+    public class CreateCarWorkshopServiceCommandValidatorTests
+    {
+        [Fact()]
+        public void Validate_WithCommand_ShouldNotHaveValidationErrors()
+        {
+            // arrange
+
+            var validator = new CreateCarWorkshopServiceCommandValidator();
+            var command = new CreateCarWorkshopServiceCommand()
+            {
+                Cost = "100 PLN",
+                Description = "Description",
+                CarWorkshopEndodedName = "Workshop1"
+            };
+
+            // act
+
+            var result = validator.TestValidate(command);
+
+            // assert
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact()]
+        public void Validate_WithInvalidCommand_ShouldHaveValidationErrors()
+        {
+            // arrange
+
+            var validator = new CreateCarWorkshopServiceCommandValidator();
+            var command = new CreateCarWorkshopServiceCommand()
+            {
+                Cost = "",
+                Description = "",
+                CarWorkshopEndodedName = null
+            };
+
+            // act
+
+            var result = validator.TestValidate(command);
+
+            // assert
+
+            result.ShouldHaveValidationErrorFor(c => c.Cost);
+            result.ShouldHaveValidationErrorFor(c => c.Description);
+            result.ShouldHaveValidationErrorFor(c => c.CarWorkshopEndodedName);
+        }
+    }
+}
