33988eb2471935ba0d0e42a5017cbc326638bea2
Muniox <muniox20@gmail.com>
2025-01-20 22:40:06 +0100

    10:21 timeslap

---
+++ CarWorkshop.Application.Tests/CarWorkshopService/Commands/CreateCarWorkshopServiceCommandHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using CarWorkshop.Application.ApplicationUser;
+using CarWorkshop.Domain.Interfaces;
+using Moq;
+using Xunit;
+
+namespace CarWorkshop.Application.CarWorkshopService.Commands.Tests
+{
+    public class CreateCarWorkshopServiceCommandHandlerTests
+    {
+        [Fact()]
+        public async Task Handle_CreateCarWorkshopService_WhenUserIsAutorized()
+        {
+            // arrange
+
+            var carWorkshop = new Domain.Entities.CarWorkshop()
+            {
+                Id = 1,
+                CreatedById = "1"
+            };
+
+            var command = new CreateCarWorkshopServiceCommand()
+            {
+                Cost = "100PLN",
+                Description = "Service description",
+                CarWorkshopEndodedName = "workshop1"
+            };
+
+            var userContextMock = new Mock<IUserContext>();
+
+            userContextMock.Setup(c => c.GetCurrentUser())
+                .Returns(new CurrentUser("1", "test@test.com", new[] { "User" }));
+
+            var carWorkshopRepositoryMock = new Mock<ICarWorkshopRepository>();
+            carWorkshopRepositoryMock.Setup(c => c.GetByEncodedName(command.CarWorkshopEndodedName))
+                .ReturnsAsync(carWorkshop);
+
+            var carWorkshopServiceRepositoryMock = new Mock<ICarWorkshopServiceRepository>();
+
+            var handler = new CreateCarWorkshopServiceCommandHandler(userContextMock.Object, carWorkshopRepositoryMock.Object, carWorkshopServiceRepositoryMock.Object);
+
+            // act
+            await handler.Handle(command, CancellationToken.None);
+
+            carWorkshopServiceRepositoryMock.Verify(m => m.Create(It.IsAny<Domain.Entities.CarWorkshopService>()), Times.Once);
+        }
+    }
+}
