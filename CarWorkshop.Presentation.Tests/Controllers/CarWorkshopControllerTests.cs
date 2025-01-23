c6400c85116b398b293446b213485bd97b2d8045
pawel.bartoszewski <pawel.bartoszewski@lge.com>
2025-01-23 15:48:21 +0100

    Mocking Controllesrs

---
+++ CarWorkshop.Presentation.Tests/Controllers/CarWorkshopControllerTests.cs
@@ -0,0 +1,102 @@
+﻿using CarWorkshop.Application.CarWorkshop;
+using CarWorkshop.Application.CarWorkshop.Queries.GetAllCarWorkShops;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Moq;
+using System.Net;
+using Xunit;
+
+namespace CarWorkshop.Presentation.Controllers.Tests
+{
+    public class CarWorkshopControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public CarWorkshopControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+
+        [Fact()]
+        public async Task Index_ReturnsViewWithExpectedData_ForExistingWorkshops()
+        {
+            // arrange
+
+            var carWorkshops = new List<CarWorkshopDto>()
+            {
+                new CarWorkshopDto()
+                {
+                    Name = "Workshop 1",
+                },
+
+                new CarWorkshopDto()
+                {
+                    Name = "Workshop 2",
+                },
+
+                new CarWorkshopDto()
+                {
+                    Name = "Workshop 3",
+                },
+            };
+
+            var mediatorMock = new Mock<IMediator>();
+
+            mediatorMock.Setup(m => m.Send(It.IsAny<GetAllCarWorkshopsQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(carWorkshops);
+
+            var client = _factory
+                .WithWebHostBuilder(builder => builder.ConfigureTestServices(services => services.AddScoped(_ => mediatorMock.Object)))
+                .CreateClient();
+
+
+            // act
+
+            var response = await client.GetAsync("/CarWorkshop/Index");
+
+            // assert
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            content.Should().Contain("<h1>Car Workshops</h1>")
+                .And.Contain("Workshop 1")
+                .And.Contain("Workshop 2")
+                .And.Contain("Workshop 3");
+        }
+
+        [Fact()]
+        public async Task Index_ReturnsEmptyView_WhenNoCarWorkshopExist()
+        {
+            // arrange
+
+            var carWorkshops = new List<CarWorkshopDto>();
+
+            var mediatorMock = new Mock<IMediator>();
+
+            mediatorMock.Setup(m => m.Send(It.IsAny<GetAllCarWorkshopsQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(carWorkshops);
+
+            var client = _factory
+                .WithWebHostBuilder(builder => builder.ConfigureTestServices(services => services.AddScoped(_ => mediatorMock.Object)))
+                .CreateClient();
+
+
+            // act
+
+            var response = await client.GetAsync("/CarWorkshop/Index");
+
+            // assert
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            content.Should().NotContain("div class=\"card m-3\"");
+        }
+    }
+}
